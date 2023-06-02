b164824f7bf27b7abb7226793088ee0b79f38bd2
유재덕 <pationcook@naver.com>
2023-06-02 19:11:00 +0900

    router hashmode로 변경
    router nuxt이용해서 ssr 로 변경하기 전까지 임시 방편

--- src/router/index.js
+++ src/router/index.js
@@ -8,5 +8,6 @@ import { headerMenuRoutes } from './headerMenu';
 export const router = new createRouter({
     //createWebHistory : vue에서 제공해주는 webHistory를 지원
     history : createWebHistory(),
+    mode: 'hash',
     routes : headerMenuRoutes
 })
